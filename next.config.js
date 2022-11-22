1003fa383240ffbcfed9550f53accbe8bb9b3fba
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-11-21 21:01:29 -0400

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
+  images: {
+    domains: ['uploads.mangadex.org'],
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import MFA from 'mangadex-full-api'
+import Image from 'next/image'
+
 export default async function HomePage() {
-  return <h1>test</h1>
+  const response = await MFA.Manga.search({ limit: 10 })
+  // MFA.resolveArray(response[0].mainCover)
+  const test = await MFA.Cover.get(response[0].mainCover.id)
+  console.log(test.image256)
+  return (
+    <div>
+      <Image src={test.image512} width={256} height={363} alt="" />
+    </div>
+  )
 }
