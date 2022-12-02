1de21c4d5e851b4207763ae7e8d6744c0bedcb93
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-12-01 22:20:24 -0400

    update

--- src/app/manga/[id]/page.tsx
+++ src/app/manga/[id]/page.tsx
@@ -1,5 +1,8 @@
+import axios, { AxiosResponse } from 'axios'
 import MFA from 'mangadex-full-api'
 import Image from 'next/image'
+import { AiOutlineFlag } from 'react-icons/ai'
+import { BiBookOpen, BiListPlus, BiStar, BiUpload } from 'react-icons/bi'
 import { MangaContainer } from './styles'
 
 type Params = {
@@ -10,17 +13,36 @@ type Props = {
   params: Params
 }
 
+interface IManga {
+  id: string
+  attributes: {
+    title: string
+    tags: []
+  }
+  relationships: []
+}
+
+type data = { data: IManga[] }
+
 export default async function Manga({ params }: Props) {
-  const responseManga = await MFA.Manga.get(params.id)
+  const responseManga = await MFA.Manga.get(params.id, true)
 
   const responseCover = await MFA.Cover.get(responseManga.mainCover.id)
 
   const responseAuthor = await MFA.Author.get(responseManga.authors[0].id)
 
+  const { data } = await axios.get<any, AxiosResponse<data, any>, any>(
+    'https://api.mangadex.org/manga?limit=10&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5BlatestUploadedChapter%5D=desc&includes%5B%5D=cover_art&includes%5B%5D=author',
+  )
+
   return (
     <MangaContainer>
       <div className="bannerBackground">
-        <div style={{ backgroundImage: `url(${responseCover.image512})` }}>
+        <div
+          style={{
+            backgroundImage: `url(${responseCover.image512})`,
+          }}
+        >
           <span className="gradient" />
         </div>
       </div>
@@ -33,6 +55,29 @@ export default async function Manga({ params }: Props) {
               {responseManga.localizedTitle.localString}
             </span>
             <span>{responseAuthor.name}</span>
+          </div>
+          <div>
+            <div>
+              <button>Add to library</button>
+              <button>
+                <BiStar />
+              </button>
+              <button>
+                <BiListPlus />
+              </button>
+              <button>
+                <BiBookOpen />
+              </button>
+              <button>
+                <AiOutlineFlag />
+              </button>
+              <button>
+                <BiUpload />
+              </button>
+            </div>
+            <div>
+              <span>{data.data[0].attributes.tags[0].attributes.name.en}</span>
+            </div>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,35 @@
-import MFA from 'mangadex-full-api'
+import axios from 'axios'
 import { MangasCarousel } from '../components/carousel'
 
 export default async function HomePage() {
-  const response = await MFA.Manga.search({
-    limit: 10,
-  })
+  // const response = await MFA.Manga.search({
+  //   limit: 10,
+  // })
   // https://api.mangadex.org/manga?includes%5B%5D=%27
   // MFA.resolveArray(response[0].mainCover)
 
   // const test = await MFA.Cover.get(response[0].mainCover.id)
 
-  const mangas = await Promise.all(
-    response.map(async (manga) => {
-      const cover = await MFA.Cover.get(manga.mainCover.id)
+  const response = await axios.get('https://api.mangadex.org/manga', {
+    params: {
+      title: '',
+      limit: 10,
+      includes: ['cover_art', 'author'],
+    },
+  })
 
-      return {
-        id: manga.id,
-        cover: cover.image256,
-        title: manga.title,
-        description: manga.description,
-      }
-    }),
-  )
+  const mangas = response.data.data.map((manga: any) => {
+    return {
+      id: manga.id,
+      cover: response.data.data[0].relationships[2].attributes.fileName,
+      title: manga,
+      description: manga.attributes.description,
+    }
+  })
+
+  // const mangas = await Promise.all(
+  //   response.map(async (manga) => {
+  //     const cover = await MFA.Cover.get(manga.mainCover.id)
 
   return (
     <div>
